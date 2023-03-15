557448c187395128f210c56e71b4f3db395a1e29
Caique Martins <caiquemartins.dev@gmail.com>
2023-03-15 10:43:40 -0300

    codigo morse retornando letras no array e sem espaço

--- codigo_morse.rb
+++ codigo_morse.rb
@@ -1,53 +1,57 @@
-letras_morse = [
-  {".-" => "A"},
-  {"-..." => "B"},
-  {"-.-." => "C"},
-  {"-.." => "D"},
-  {"." => "E"},
-  {"..-." => "F"},
-  {"--." => "G"},
-  {"...." => "H"},
-  {".." => "I"},
-  {".---" => "J"},
-  {"-.-" => "K"},
-  {".-.." => "L"},
-  {"--" => "M"},
-  {"-." => "N"},
-  {"---" => "O"},
-  {".--." => "P"},
-  {"--.-" => "Q"},
-  {".-." => "R"},
-  {"..." => "S"},
-  {"-" => "T"},
-  {"..-" => "U"},
-  {"...-" => "V"},
-  {".--" => "W"},
-  {"-..-" => "X"},
-  {"-.--" => "Y"},
-  {"--.." => "Z"},
-  {"" => " "}
-]
+
 
 codigomorse = '.... . -.--   .--- ..- -.. .'
 
-def morse (codigomorse)
+def morse(codigomorse)
   palavras_morse = codigomorse.split(" ")
   frasetraduzida = []
 
 
-  contador = 0
+ # contador = 0
 
-  palavras_morse.length.times do
+  ((palavras_morse.length)+1).times do |i|
 
-    traduzir_palavras = palavras_morse[contador]
-    palavra_traduzida = letras_morse.find{ |p|[traduzir_palavras]}
+    letras_morse = [
+      {".-" => "A"},
+      {"-..." => "B"},
+      {"-.-." => "C"},
+      {"-.." => "D"},
+      {"." => "E"},
+      {"..-." => "F"},
+      {"--." => "G"},
+      {"...." => "H"},
+      {".." => "I"},
+      {".---" => "J"},
+      {"-.-" => "K"},
+      {".-.." => "L"},
+      {"--" => "M"},
+      {"-." => "N"},
+      {"---" => "O"},
+      {".--." => "P"},
+      {"--.-" => "Q"},
+      {".-." => "R"},
+      {"..." => "S"},
+      {"-" => "T"},
+      {"..-" => "U"},
+      {"...-" => "V"},
+      {".--" => "W"},
+      {"-..-" => "X"},
+      {"-.--" => "Y"},
+      {"--.." => "Z"},
+      {"" => " "}
+    ]
+
+    traduzir_palavras = palavras_morse[i]
+    palavra_traduzida = letras_morse.find{ |p| p[traduzir_palavras] }.values.first
 
     frasetraduzida.push(palavra_traduzida)
 
 
-    puts palavra_traduzida
-    puts frasetraduzida
+    puts "palavras traduzida: #{palavra_traduzida}"
+    puts  "frasetraduzida: #{frasetraduzida}"
 
-    contador = contador + 1
+#    contador = contador + 1
   end
 end
+
+morse(codigomorse)
